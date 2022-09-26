daed5e6320c1dad699357901b95e2e526a2c034c
DESKTOP-J9U7SOI\Fernando Bayona <bayona.develop@gmail.com>
2022-09-26 01:16:43 -0500

    no message

---
+++ services/src/test/kotlin/com/bayonasoftware/batteryplus/microservices/services/services/ServicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bayonasoftware.batteryplus.microservices.services.services
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServicesApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
